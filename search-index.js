d8a7569a1d27d859ddd68dab8579ba7d17e7497b
Shfty <Shfty@users.noreply.github.com>
2023-03-02 05:41:09 +0000

    deploy: 396b3e07fa0766b7f5593c97a484c7330965f864

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"permutate_macro":{"doc":"Procedural macro for permutating functions at compile time.","t":"X","n":["permutate"],"q":["permutate_macro"],"d":["Generates conditional permutations of a function based on …"],"i":[0],"f":[0],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"permutate_macro":["",[["permutate",[],["attributes.rs","keywords.rs","mod.rs","parameter_conditional.rs","parameters.rs","permutate_attribute.rs","permutations.rs"]]],["lib.rs"]]\
+}');
+createSourceSidebar();
