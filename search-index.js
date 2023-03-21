b1dd4e0770d5cdcf8188a1ddcdf3e408169f7caa
Shfty <Shfty@users.noreply.github.com>
2023-03-21 04:01:42 +0000

    deploy: b8fc965b3a77de11aef77aadc7cbcbbdba3eecb1

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"permutate_macro":{"doc":"Procedural macro for permutating functions at compile time.","t":"X","n":["permutate"],"q":["permutate_macro"],"d":["Generates conditional permutations of a function based on …"],"i":[0],"f":[0],"p":[]}\
+"permutate_macro":{"doc":"Procedural macro for permutating functions at compile time.","t":"X","n":["permutate"],"q":[[0,"permutate_macro"]],"d":["Generates conditional permutations of a function based on …"],"i":[0],"f":[0],"c":[],"p":[]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
